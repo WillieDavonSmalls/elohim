9f89b4497473c13d3fe81f8327c6029556b61f25
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-08-05 21:39:17 -0400

    updates

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl, Table, Navbar, NavItem, Nav } from 'react-bootstrap';
+import { Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl, Table, Button } from 'react-bootstrap';
 
 import './home.css'
 
@@ -23,6 +23,27 @@ export default class Home extends Component {
         this.setState({ value: e.target.value });
     }
 
+    //Button
+    // constructor(props, context) {
+    //   super(props, context);
+
+    //   this.handleClick = this.handleClick.bind(this);
+
+    //   this.state = {
+    //     isLoading: false
+    //   };
+    // }
+
+    handleClick() {
+        this.setState({ isLoading: true });
+
+        // This probably where you would have an `ajax` call
+        setTimeout(() => {
+            // Completed of async action, set loading state back
+            this.setState({ isLoading: false });
+        }, 2000);
+    }
+    //////////////
 
     render() {
 
@@ -100,52 +121,85 @@ export default class Home extends Component {
                         <Row>
                             <Col xs={12} md={12}>
                                 <h5>FINANCIAL INFORMATION:</h5>
-                                    <Col xs={12} md={6}>
-                                        <p><i><strong>Coverage Values:</strong></i></p>
-                                        <p>Coverage A: 1,000,000</p>
-                                        <p>Coverage B: 1,000,000</p>
-                                        <p>Coverage C: 1,000,000</p>
-                                        <p>Coverage D: 1,000,000</p>
-                                        <p>Coverage E: 1,000,000</p>
-                                        <p>Coverage F: 1,000,000</p>
-                                        <p>Total Insured Value (TIV): 1,000,000</p>
-                                    </Col>
-                                    <Col xs={12} md={6}>
-                                        <p><i><strong>Deductible and Flood Policy Information:</strong></i></p>
-                                        <p>Wind: 2%</p>
-                                        <p>Earthquake: 0%</p>
-                                        <p>WS Site Ded as % of A: 2.63%</p>
-                                        <p>AOP Deductible: 10,000</p>
-                                        <p>Primary Flood Insurance Policy: Unknown</p>
-                                    </Col>
+                                <Col xs={12} md={6}>
+                                    <p><i><strong>Coverage Values:</strong></i></p>
+                                    <p>Coverage A: 1,000,000</p>
+                                    <p>Coverage B: 1,000,000</p>
+                                    <p>Coverage C: 1,000,000</p>
+                                    <p>Coverage D: 1,000,000</p>
+                                    <p>Coverage E: 1,000,000</p>
+                                    <p>Coverage F: 1,000,000</p>
+                                    <p>Total Insured Value (TIV): 1,000,000</p>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <p><i><strong>Deductible and Flood Policy Information:</strong></i></p>
+                                    <p>Wind: 2%</p>
+                                    <p>Earthquake: 0%</p>
+                                    <p>WS Site Ded as % of A: 2.63%</p>
+                                    <p>AOP Deductible: 10,000</p>
+                                    <p>Primary Flood Insurance Policy: Unknown</p>
+                                </Col>
                             </Col>
                         </Row>
 
-                        {/* Financial Information */}
-                            <Col xs={12} md={12}>
-                                <Row>
+                        {/* Exposure Information */}
+                        <Col xs={12} md={12}>
+                            <Row>
                                 <h5>EXPOSURE INFORMATION:</h5>
-                                    <Col xs={12} md={6}>
-                                        <p><i><strong>Primary Characteristics:</strong></i></p>
-                                        <p>Construction: Frame</p>
-                                        <p>Number of Stories: 2</p>
-                                        <p>Year Built: 1900</p>
-                                        <p>Floor Area: 6,000</p>
-                                    </Col>
-                                    <Col xs={12} md={6}>
-                                        <p><i><strong>Secondary Characteristics:</strong></i></p>
-                                        <p>Open Protection: Other/Unknown</p>
-                                        <p>Roof Anchor: Other/Unknown</p>
-                                        <p>Roof Geometry: Unknown</p>
-                                        <p>Roof Sheathing: Other/Unknown</p>
-                                        <p>Roof Age: 11 years or more</p>
-                                        <p>Year Upgrade: Unknown</p>
-                                    </Col>
-                                </Row>
-                            </Col>
-                        <Row> 
-                            
-                        </Row>
+                                <Col xs={12} md={6}>
+                                    <p><i><strong>Primary Characteristics:</strong></i></p>
+                                    <p>Construction: Frame</p>
+                                    <p>Number of Stories: 2</p>
+                                    <p>Year Built: 1900</p>
+                                    <p>Floor Area: 6,000</p>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <p><i><strong>Secondary Characteristics:</strong></i></p>
+                                    <p>Open Protection: Other/Unknown</p>
+                                    <p>Roof Anchor: Other/Unknown</p>
+                                    <p>Roof Geometry: Unknown</p>
+                                    <p>Roof Sheathing: Other/Unknown</p>
+                                    <p>Roof Age: 11 years or more</p>
+                                    <p>Year Upgrade: Unknown</p>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                        {/* comment section */}
+                        {/* Exposure Information */}
+                        <Col xs={12} md={12}>
+                            <Row>
+                                <h5>COMMENTS SECTION:</h5>
+                                {/* current comments */}
+                                <Col xs={12} md={6}>
+
+                                </Col>
+                                {/* New comments */}
+                                <Col xs={12} md={6}>
+                                    <Form>
+                                        <FormGroup controlId="formCommentsText">
+                                            <ControlLabel>New Comments:</ControlLabel>
+                                            <FormControl
+                                                style={{ height: '100px' }}
+                                                type="text"
+                                                value={this.state.value}
+                                                placeholder="Enter text"
+                                                onChange={this.handleChange}
+                                            />
+                                            <FormControl.Feedback />
+                                        </FormGroup>
+                                    </Form>
+                                    <Button bsStyle="primary"> Submit
+                                        {/* bsStyle="primary"
+                                        disabled={isLoading}
+                                        onClick={!isLoading ? this.handleClick : null}>
+                                        {isLoading ? 'Submitting...' : 'Submit'} */}
+                                    </Button>
+
+
+                                </Col>
+                            </Row>
+                        </Col>
                     </Col>
                 </Row>
             </Grid>
