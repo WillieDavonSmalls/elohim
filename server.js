ace50253506d5a843c1a3fd1a6a05fceb724b170
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-08-07 19:12:31 -0400

    applicaiton working with node now

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(bodyParser.json());
-
 
 // Import routes and give the server access to them.
 const routes = require("./controllers/controllers_bordereau.js");
 
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+
+app.use(express.static("client/build"));
+
 app.use(routes);
 
+
+// // Send every other request to the React app
+// // Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
