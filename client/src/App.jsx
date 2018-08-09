84642f329ab95b2ac0ef53d72d0dbdb706d09044
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-08-08 23:31:42 -0400

    user authentication updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Navbar from './views/home/home_navbar'
-import home from './views/home/home';
-import about from './views/home/about';
-import contact from './views/home/contact';
+import Home from './views/home/home';
+import About from './views/home/about';
+import Contact from './views/home/contact';
 import './App.css';
 
+// TODO: MOve to login.jsx
 class Login extends Component {
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.props.handleLogIn}>
           <label>Username: </label>
           <input type="text" placeholder="Enter username" />
           <label>Password: </label>
           <input type="password" placeholder="Enter password" />
+          <button type="submit">login</button>
         </form>
       </div>
     );
@@ -26,8 +28,16 @@ class App extends Component {
     super(props);
 
     this.state = {
-      loggedIn: false
+      loggedIn: true
     }
+
+    this.handleLogIn = this.handleLogIn.bind(this)
+  }
+
+  handleLogIn() {
+    // fetch("/api/validateLogin")
+    // .then(response => response.json())
+    // .then(data => (console.log(data), this.setState({loggedIn: data})))
   }
 
   render() {
@@ -37,10 +47,11 @@ class App extends Component {
       <Router>
         <div> 
           <Navbar/>
-          <Route exact path="/login" component ={Login} />
-          <Route exact path="/" component ={home} />
-          <Route exact path="/about" component ={about} />
-          <Route exact path="/contact" component ={contact} />
+          <Route exact path="/login" render ={() => <Login handleLogIn={this.handleLogIn} />} />
+          {/* <Route exact path="/signin" render ={() => <Login handleLogIn={this.handleLogIn} />} /> */}
+          <Route exact path="/" render ={() => isLoggedIn ? <Home/> : <Redirect to="/login" />} />
+          <Route exact path="/about" render ={() => isLoggedIn ? <About/> : <Redirect to="/login" />} />
+          <Route exact path="/contact" render ={() => isLoggedIn ? <Contact/> : <Redirect to="/login" />} />
         </div>
       </Router>
     );
